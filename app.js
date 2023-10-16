7f43fcec1f7245e1a291195c6e886db3fabed35a
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-16 04:56:42 -0700

    setup mysql with basic get games endpoint

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const mysql = require('mysql')
+const config = require('config')
+
+const dbConfig = config.get('TicTacToe.dbConfig')
+const pool = mysql.createPool(dbConfig)
 
 const app = express()
 app.use(express.json())
@@ -9,6 +14,18 @@ app.get("/status", (request, response) => {
     }
     
     response.send(status)
+})
+
+app.get("/games", (request, response) => {
+    pool.getConnection(function(err, con) {
+        if (err) throw err
+        console.log("Connected!")
+        con.query("SELECT * FROM game", function (err, result) {
+            if (err) throw err
+            console.log("Result: " + result)
+            response.send(result)
+        })
+    })
 })
 
 const PORT = process.env.PORT || 5000
