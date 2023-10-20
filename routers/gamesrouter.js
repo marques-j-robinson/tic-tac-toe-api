318c4b791806d751924b0c5bdc28253141cb1e8c
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-20 00:31:50 -0700

    wrap all routes in a try/catch statement

--- routers/gamesrouter.js
+++ routers/gamesrouter.js
@@ -3,33 +3,37 @@ const { getSingle, getAll, create, remove } = require('../models/game.js')
 const app = Router()
 
 app.get("/games", async (req, res) => {
-    const { gameId } = req.query
-    if (gameId) {
-        const game = await getSingle(gameId)
+    try {
+        const { gameId } = req.query
+        if (gameId) {
+            const game = await getSingle(gameId)
+            if (!game) throw Error('GAME_NOT_FOUND')
+            return res.send({
+                success: true,
+                data: game
+            })
+        }
+        const games = await getAll()
         return res.send({
             success: true,
-            data: game
+            data: games
         })
+    } catch (e) {
+        return res.status(500).send({"success": false, "msg": e.message})
     }
-    const games = await getAll()
-    return res.send({
-        success: true,
-        data: games
-    })
 })
 
 app.post("/games", async (req, res) => {
-    if (!req.body.name) {
-        return req.status(400).send({
-            success: false,
-            msg: "Parameter 'name' is missing from request body!"
+    try {
+        if (!req.body.name) throw Error('REQ_PARAM_MISSING')
+        const games = await create(req.body.name)
+        return res.send({
+            success: true,
+            data: games
         })
+    } catch (e) {
+        return res.status(500).send({"success": false, "msg": e.message})
     }
-    const games = await create(req.body.name)
-    return res.send({
-        success: true,
-        data: games
-    })
 })
 
 app.delete("/games/:gameId", async (req, res) => {
