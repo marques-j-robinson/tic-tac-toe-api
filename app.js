d5d40b65f4721f816a542b7e63cd12aaff167281
marques-j-robinson <marques.j.robinson@gmail.com>
2023-10-26 04:16:37 -0700

    remove all endpoints to rebuild api

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const express = require('express')
 const config = require('config')
 const ErrorHandler = require('./middlewares/ErrorHandler.js')
-const {
-    create: createGame,
-    getAll: getAllGames,
-    getGame,
-    remove: removeGame,
-    update: updateGameHistory,
-} = require('./models/games.js')
 
 const app = express()
 
@@ -21,65 +14,6 @@ app.use(express.json())
 app.set('json spaces', 2)
 
 // Routes
-app.get('/games', async (req, res, next) => {
-    try {
-        const games = await getAllGames()
-        return res.send({
-            success: true,
-            data: { games }
-        })
-    } catch (err) {
-        next(err)
-    }
-})
-
-app.post('/game', async (req, res, next) => {
-    try {
-        const {insertId:id} = await createGame()
-        return res.send({
-            success: true,
-            data: { id }
-        })
-    } catch (err) {
-        next(err)
-    }
-})
-
-app.get('/game/:id', async (req, res, next) => {
-    try {
-        const {id} = req.params
-        const game = await getGame(id)
-        return res.send({
-            success: true,
-            data: { game }
-        })
-    } catch (err) {
-        next(err)
-    }
-})
-
-app.delete('/game/:id', async (req, res, next) => {
-    try {
-        const {id} = req.params
-        await removeGame(id)
-        return res.send({ success: true })
-    } catch (err) {
-        next(err)
-    }
-})
-
-app.put('/game/:id', async (req, res, next) => {
-    try {
-        const {id} = req.params
-        const {affectedRows} = await updateGameHistory(id, req.body.history)
-        return res.send({
-            success: true,
-            data: {affectedRows}
-        })
-    } catch (err) {
-        next(err)
-    }
-})
 
 // ERROR HANDLER MIDDLEWARE
 app.use(ErrorHandler)
